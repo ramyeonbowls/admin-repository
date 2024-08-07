4d6e5741515d9c6a70bb9eeda21f6250230a70e5
Julian Putra <julian.putr99@gmail.com>
2024-08-07 23:17:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'register' => true,
+    'verify' => false,
+    'confirm' => true,
+    'reset' => true
+]);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
