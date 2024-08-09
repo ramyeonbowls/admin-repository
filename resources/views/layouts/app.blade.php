d3209df189cbb6ffebb65b7670325f8ca63c14d0
Julian <julian@eskalink.com>
2024-08-09 18:02:41 +0700

    Update themes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,105 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    class="light-style layout-wide customizer-hide"
+    dir="ltr"
+    data-theme="theme-default"
+    data-assets-path="../../../public/"
+    data-template="vertical-menu-template-free"
+    data-style="light">
+    <head>
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+        <!-- Favicon -->
+        <link
+            rel="icon"
+            type="image/x-icon"
+            href="{{ asset('img/favicon/favicon.ico?' . rand(10000, 99999)) }}" />
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+            rel="stylesheet" />
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+        <!-- Css -->
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="{{ asset('vendor/fonts/boxicons.css?' . rand(10000, 99999)) }}" />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="{{ asset('vendor/css/core.css?' . rand(10000, 99999)) }}"
+            class="template-customizer-core-css" />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="{{ asset('vendor/css/theme-default.css?' . rand(10000, 99999)) }}"
+            class="template-customizer-theme-css" />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="{{ asset('css/demo.css?' . rand(10000, 99999)) }}" />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.css?' . rand(10000, 99999)) }}" />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="{{ asset('vendor/css/pages/page-auth.css?' . rand(10000, 99999)) }}" />
+        @yield('css')
 
-                    </ul>
+        <!-- Scripts -->
+        <script
+            type="text/javascript"
+            src="{{ asset('vendor/js/helpers.js?' . rand(10000, 99999)) }}"></script>
+        <script
+            type="text/javascript"
+            src="{{ asset('js/config.js?' . rand(10000, 99999)) }}"></script>
+        @yield('js')
+        @vite(['resources/sass/app.scss', 'resources/sass/core.scss', 'resources/sass/theme-default.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div id="app-container">
+            <main id="main-container">
+                @yield('content')
+            </main>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+            <!-- Charts JS -->
+            <script
+                type="text/javascript"
+                src="{{ asset('vendor/libs/jquery/jquery.js?' . rand(10000, 99999)) }}"></script>
+            <script
+                type="text/javascript"
+                src="{{ asset('vendor/libs/popper/popper.js?' . rand(10000, 99999)) }}"></script>
+            <script
+                type="text/javascript"
+                src="{{ asset('vendor/js/bootstrap.js?' . rand(10000, 99999)) }}"></script>
+            <script
+                type="text/javascript"
+                src="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.js?' . rand(10000, 99999)) }}"></script>
+            <script
+                type="text/javascript"
+                src="{{ asset('vendor/js/menu.js?' . rand(10000, 99999)) }}"></script>
+            <script
+                type="text/javascript"
+                src="{{ asset('js/main.js?' . rand(10000, 99999)) }}"></script>
+            <script
+                async
+                defer
+                src="https://buttons.github.io/buttons.js"></script>
+            @yield('javascript')
+        </div>
+    </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/core.scss',
+                'resources/sass/theme-default.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
@@ -23,6 +26,9 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
+            '@': path.resolve(__dirname, 'resources/sass'),
+            '@components': path.resolve(__dirname, 'resources/sass/_components'),
+            '@custom-variables': path.resolve(__dirname, 'resources/sass/_custom-variables'),
         },
     },
 });
